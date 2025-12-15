6e2bef66b26d4ff0493a6d765e56d411d651a531
Parashu Kalakutagi <nagaraj@Parashu.local>
2025-12-15 09:55:51 +0530

    sec

--- ch_1/app/main.py
+++ ch_1/app/main.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +7,19 @@ app = FastAPI()
 @app.get("/product/{product_id}")
 async def products(product_id:int):
     return {"product_id": product_id,"response":"all products are here"}
+
+@app.post("/product")
+async def create_product(product:dict):
+    return {"products": product}
+
+@app.put("/product/{product_id}")
+async def update_product(product:dict, product_id:int):
+    return {"products": product}
+
+@app.delete("/product/{product_id}")
+async def delete_product(product_id:int):
+    return {"products": product}
+
+@app.patch("/product/{product_id}")
+async def update_product(product:dict, product_id:int):
+    return {"products": product}
